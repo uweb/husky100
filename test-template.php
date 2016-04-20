29b2f3581f6bde1383a6d13ae8fa1cc20b7941c5
Nick Winkelbauer <nwinkelb@uw.edu>
2016-04-20 15:48:05 -0700

    test

--- test-template.php
+++ test-template.php
@@ -162,11 +162,7 @@
            //gather assets
            $personimageurl = wp_get_attachment_image_src( get_post_thumbnail_id($person->ID) , $size = ['200','300'] )[0];
            $personimageurlhigh = wp_get_attachment_image_src( get_post_thumbnail_id($person->ID) , $size = 'large' )[0];
-           if ( !$personimageurl ) {
-            //set to default image here
-            $personimageurl = plugin_dir_url( __FILE__ ) . 'assets/default.jpg';
-            $personimageurlhigh = plugin_dir_url( __FILE__ ) . 'assets/default.jpg';
-           }
+           
            
 
            //spit out html 
