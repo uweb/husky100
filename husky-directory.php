84982dcca45ae2ea4b88a166caef92efdafc43ac
Nick Winkelbauer <nwinkelb@uw.edu>
2016-04-20 14:34:09 -0700

    Fixes add_husky_directory_template

--- husky-directory.php
+++ husky-directory.php
@@ -151,8 +151,8 @@ if ( ! post_type_exists( 'husky100' ) ):
 			else if (file_exists($this_dir . '/' . $husky_directory_template)) {
 				if( strpos( $current_url , "washington.edu" ) !== false ){
 					//print_r($this_dir);
-					list( $server_path , $web_path ) = explode( $this_dir , "/cms/" );
-					$this_dir = "http://www.washington.edu/cms/" . $web_path;
+					$web_path = explode( $this_dir , "/cms/" );
+					$this_dir = "http://www.washington.edu/cms/" . $web_path[1];
 				}
 				print_r("Dir: " . $this_dir . '/' . $husky_directory_template . "</br>");
 				return $this_dir . '/' . $husky_directory_template;
