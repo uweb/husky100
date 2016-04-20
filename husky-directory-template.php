71315745e8eff214cb316056d54c6e260a4c937a
Nick Winkelbauer <nwinkelb@uw.edu>
2016-04-20 15:31:04 -0700

    test

--- husky-directory-template.php
+++ husky-directory-template.php
@@ -1,9 +1,3 @@
-<?php
-/*
-Template Name: Husky 100
-*/
-?>
-
 <!DOCTYPE html>
 <html>
     <head>
@@ -31,14 +25,14 @@ Template Name: Husky 100
     <div id="thin-strip">
         <a class="wordmark" href="http://uw.edu" tabindex="-1" title="University of Washington Home">Home</a>
         <ul>
-            <li class="facebook"><a href="https://www.facebook.com/UofWA" title="Facebook">Facebook</a>
-            <li class="twitter"><a href="https://twitter.com/uw" title="Twitter">Twitter</a>
-            <li class="instagram"><a href="http://instagram.com/uofwa" title="Instagram">Instagram</a>
-            <li class="youtube"><a href="http://www.youtube.com/uwhuskies" title="YouTube">YouTube</a>
-            <li><a href="http://uw.edu/students" class="slash" title="Students">Students</a>
-            <li><a href="http://uw.edu/parents" class="slash" title="Parents">Parents</a>
-            <li><a href="http://uw.edu/facultystaff" class="slash" title="Faculty &amp; Staff">Faculty &amp; Staff</a>
-            <li><a href="http://uw.edu/alumni" class="slash" title="Alumi">Alumni</a>
+            <li class="facebook"><a href="https://www.facebook.com/UofWA" title="Facebook">Facebook</a></li>
+            <li class="twitter"><a href="https://twitter.com/uw" title="Twitter">Twitter</a></li>
+            <li class="instagram"><a href="http://instagram.com/uofwa" title="Instagram">Instagram</a></li>
+            <li class="youtube"><a href="http://www.youtube.com/uwhuskies" title="YouTube">YouTube</a></li>
+            <li><a href="http://uw.edu/students" class="slash" title="Students">Students</a></li>
+            <li><a href="http://uw.edu/parents" class="slash" title="Parents">Parents</a></li>
+            <li><a href="http://uw.edu/facultystaff" class="slash" title="Faculty &amp; Staff">Faculty &amp; Staff</a></li>
+            <li><a href="http://uw.edu/alumni" class="slash" title="Alumi">Alumni</a></li>
         </ul>
     </div>
  
