41794b2f444c0671a483fff35bae61e4820ff5ed
Nick Winkelbauer <nwinkelb@uw.edu>
2016-04-20 15:39:39 -0700

    test

--- test-template.php
+++ test-template.php
@@ -183,44 +183,23 @@
                 $personclasses .= "featured ";
            }
 
-    
+           if( $peoplecount % $featureOffset == 9 ) { //determines where fast facts are
+            $fact = $fastfacts[$peoplecount / $featureOffset];
+            $factimageurl = wp_get_attachment_image_src( get_post_thumbnail_id($fact->ID) , $size = ['200','300'] )[0];
+            $factimageurlhigh = wp_get_attachment_image_src( get_post_thumbnail_id($fact->ID) , $size = 'large' )[0];
+            ?>
+                <div data-name="<?php echo $fact->post_name; ?>" class="flip-container grid-item special infographic">
+                    <div >
+                      <div class="front">
+                        <img src="<?php echo $factimageurlhigh; ?>" alt="<?php echo $fact->post_title; ?>">
+                      </div>
+                    </div>
+                  </div>
+            <?php
+           }
 
            //spit out html 
-           ?>
-            <div tabindex="0" data-name="<?php echo $person->post_name; ?>" data-img="<?php echo $personimageurlhigh; ?>" class="flip-container grid-item <?php echo $personclasses; ?>">
-            <div class="flipper">
-              <div class="front" style="<?php echo 'background-image:url(' . $personimageurl . ');'; ?> ">
-              </div>
-              <div class="back">
-                <h3><?php echo $person->post_title; ?></h3>
-                <!-- <p><?php echo $hometown; ?></p> -->
-                <p class="major"><?php echo $major; ?></p>
-                <p><?php echo $graduation; ?></p>    
-              </div>
-              <div class="full-bio">
-                <h2><?php echo $person->post_title; ?></h2>
-                <div class="bio-info">
-                  <p><?php echo $hometown; ?></p>
-                  <p><?php echo $major; ?></p>
-                  <p><?php echo $graduation; ?></p>                  
-                  <a class='linkedin' href="<?php echo $linkedin; ?>">LinkedIn</a>
-                </div>
-                <div class="bio-text">
-                  <p><?php echo $person->post_content; ?></p>
-                </div>
-                <div class="tags">
-                <?php foreach ($tags as $tag ) {
-                    echo '<a href="#">' . $tag->name . '</a>';
-                } ?>
-                </div>
-              </div>
-            </div>
-          </div>
-
-
-
-
-        <?php
+           
             $peoplecount++;
         }
 
