396ec1b111a648e9caec3a69f49dafb1f2201ffd
Nick Winkelbauer <nwinkelb@uw.edu>
2016-04-27 11:17:31 -0700

    fixed linkin to absolute links

--- husky-directory-template.php
+++ husky-directory-template.php
@@ -181,8 +181,8 @@
            $major = get_post_meta($person->ID, 'major', true);
            $minor = get_post_meta($person->ID, 'minor', true);
            $linkedin = get_post_meta($person->ID, 'linkedin', true);
-           if ( strpos($linkedin, 'http') !== false ) {
-             $linkedin = "http://" . $linkedin;
+           if ( strpos($linkedin, 'http') == false ) {
+             $linkedin = "https://" . $linkedin;
            }
            $filters = wp_get_post_terms( $person->ID, 'filters' );
            $tags = wp_get_post_terms( $person->ID, 'tags' );
