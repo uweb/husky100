bed1bba48a4c8fe4c44c56ecb95ed29f2f693132
Nick Wisehart <nwinkelb@uw.edu>
2018-06-27 19:07:36 -0700

    Bug fix: running lazy load before isotope for featured image change

--- js/tiles.dev.js
+++ js/tiles.dev.js
@@ -2,6 +2,19 @@ $(window).load(function(){
 
     // $( function() {
 
+
+ // lazyloading (not IE) in 3... 2... 1...
+  // var ua = window.navigator.userAgent;
+  // var msie = ua.indexOf("MSIE ");
+  // if (msie < 0) // If NOT Internet Explorer
+  // {
+      var myLazyLoad = new LazyLoad({
+          elements_selector: ".lazy"
+      });
+  // }
+
+
+
       var qsRegex,
           $searcher_wrap = $( "#searcher_wrap" ),
           $searcher = $( "#searcher" ),
@@ -227,16 +240,6 @@ $(window).load(function(){
 
    //  });
 
-
- // lazyloading (not IE) in 3... 2... 1...
-  // var ua = window.navigator.userAgent;
-  // var msie = ua.indexOf("MSIE ");
-  // if (msie < 0) // If NOT Internet Explorer
-  // {
-      var myLazyLoad = new LazyLoad({
-          elements_selector: ".lazy"
-      });
-  // }
 
 });  
 
